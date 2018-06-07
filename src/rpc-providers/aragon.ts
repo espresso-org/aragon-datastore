8dd8a9383b5934a49a5ff9275beebb4cff5cea0e
Maxime Cote <maxime.cote184@gmail.com>
2018-06-07 12:30:40 -0400

    Begin Aragon rpc provider

--- src/rpc-providers/aragon.ts
+++ src/rpc-providers/aragon.ts
@@ -1,3 +1,5 @@
+import {BigNumber} from 'bignumber.js'
+
 export class Aragon {
 
     private _aragonApp
@@ -6,10 +8,38 @@ export class Aragon {
         this._aragonApp = aragonApp
     }
 
-    async addFile(storageRef, fileSize, isPublic) {
+    async getContract() {
+        return new AragonContract(this._aragonApp)
+    }
+}
+
+
+export class AragonContract {
+
+    private _aragonApp
+
+    constructor(aragonApp) { 
+        this._aragonApp = aragonApp
+    }
+
+    async addFile(storageRef, name, fileSize, isPublic) {
         return new Promise((res, rej) => {
-            this._aragonApp.addFile(storageRef, fileSize, isPublic)
+            this._aragonApp.addFile(storageRef, name, fileSize, isPublic)
                 .subscribe(result => res(result))
+        })
+    }
+
+    async getFile(fileId) {
+        return new Promise((res, rej) => {
+            this._aragonApp.call('getFile', fileId)
+                .subscribe(result => res(result))
+        })
+    }   
+    
+    async lastFileId() {
+        return new Promise((res, rej) => {
+            this._aragonApp.call('lastFileId')
+                .subscribe(result => res(new BigNumber(result)))
         })
     }
 }
