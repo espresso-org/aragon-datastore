36595158c99025459a080db9c9c42057fdc1062e
Mathew Cormier <mathew.corm@gmail.com>
2018-08-08 19:31:53 -0400

    Add returned types

--- src/rpc-providers/rpc-provider-contract.ts
+++ src/rpc-providers/rpc-provider-contract.ts
@@ -1,15 +1,16 @@
-import { getDiffieHellman } from "crypto";
+import { BigNumber } from 'bignumber.js'
+import { Observable } from 'rxjs'
 
 export interface RpcProviderContract {
     
-    lastFileId(): Promise<any>
-    addFile(storageRef: string, name: string, fileSize: number, isPublic: boolean): Promise<any>
-    getFile(fileId: number): Promise<any>
-    deleteFile(fileId: number): Promise<any>
-    setFilename(fileId: number, newName: string): Promise<any>
-    setFileContent(fileId: number, storageRef: string, fileSize: number): Promise<any>
-    getPermissionAddresses(fileId: number): Promise<any>
+    lastFileId(): Promise<BigNumber>
+    addFile(storageRef: string, name: string, fileSize: number, isPublic: boolean): Promise<{}>
+    getFile(fileId: number): Promise<any[]>
+    deleteFile(fileId: number): Promise<{}>
+    setFilename(fileId: number, newName: string): Promise<{}>
+    setFileContent(fileId: number, storageRef: string, fileSize: number): Promise<{}>
+    getPermissionAddresses(fileId: number): Promise<string[]>
     getPermission(fileId: number, entity: string): Promise<any>
-    setWritePermission(fileId: number, entity: string, hasWritePermission: boolean): Promise<any>
-    events(...args): Promise<any>
+    setWritePermission(fileId: number, entity: string, hasWritePermission: boolean): Promise<{}>
+    events(...args): Promise<Observable<any>>
 }
