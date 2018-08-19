ef6e7827bcfb64afb9366fae9d6c6e7a4cb8326a
Maxime Cote <maxime.cote184@gmail.com>
2018-08-19 12:09:21 -0400

    addFile pin test

--- src/storage-providers/ipfs.spec.ts
+++ src/storage-providers/ipfs.spec.ts
@@ -1,4 +1,5 @@
 import { Ipfs } from './ipfs' 
+import * as ipfsAPI from 'ipfs-api' 
 
 const IpfsConfig = {
     host: 'localhost', 
@@ -7,17 +8,21 @@ const IpfsConfig = {
 }
 
 describe('IPFS Provider', async () => {
-
+    const _ipfs = new ipfsAPI(IpfsConfig)
 
     describe('files', async () => {
 
-        it('add file', async () => {
+        describe('addFile', async () => {
+            it('pins file', async () => {
 
-            let ipfs = new Ipfs(IpfsConfig)
-            let result = await ipfs.addFile(new ArrayBuffer(40))
-            console.log('result: ', result) 
-            expect(null).toBeNull()
+                let ipfs = new Ipfs(IpfsConfig)
+                let result = await ipfs.addFile(new ArrayBuffer(40))
 
+                let pinnedHash = (await _ipfs.pin.ls()).find(pin => pin.hash === result)
+                
+                expect(pinnedHash.hash).toEqual(result)
+
+            }, 30000)
         })
 
         xit('get file', async () => {
