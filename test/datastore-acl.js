721c49a822d0d4b271a7bf0ba8d94357845758dc
Mathew Cormier <mathew.corm@gmail.com>
2018-10-08 11:44:14 -0400

    Add datastore-acl tst

---
+++ test/datastore-acl.js
@@ -0,0 +1,96 @@
+const _ = require('lodash')
+
+const Datastore = artifacts.require('Datastore')
+const DatastoreACL = artifacts.require('DatastoreACL')
+const DAOFactory = artifacts.require('@aragon/core/contracts/factory/DAOFactory')
+const EVMScriptRegistryFactory = artifacts.require('@aragon/core/contracts/factory/EVMScriptRegistryFactory')
+const ACL = artifacts.require('@aragon/core/contracts/acl/ACL')
+const Kernel = artifacts.require('@aragon/core/contracts/kernel/Kernel')
+
+contract('DatastoreACL ', accounts => {
+    let datastore
+    let daoFact
+    let acl
+    let kernel
+    let kernelBase
+    let aclBase
+    let APP_MANAGER_ROLE
+    let datastoreACL
+
+    const root = accounts[0]
+    const holder = accounts[1]
+
+
+    before(async () => {
+        aclBase = await ACL.new()        
+        kernelBase = await Kernel.new(true)
+    })
+
+    beforeEach(async () => {
+        
+        const regFact = await EVMScriptRegistryFactory.new()
+        daoFact = await DAOFactory.new(kernelBase.address, aclBase.address, regFact.address)        
+
+        const r = await daoFact.newDAO(root)
+        kernel = Kernel.at(r.logs.filter(l => l.event == 'DeployDAO')[0].args.dao)
+        acl = ACL.at(await kernel.acl())         
+        
+        APP_MANAGER_ROLE = await kernelBase.APP_MANAGER_ROLE()
+
+        await acl.createPermission(holder, kernel.address, APP_MANAGER_ROLE, holder, { from: root })
+
+        const receipt = await kernel.newAppInstance('0x1234', (await Datastore.new()).address, { from: holder })
+        
+        datastore = Datastore.at(receipt.logs.filter(l => l.event == 'NewAppProxy')[0].args.proxy)
+
+        await acl.createPermission(root, datastore.address, await datastore.DATASTORE_MANAGER_ROLE(), root)
+        await acl.grantPermission(root, datastore.address, await datastore.DATASTORE_MANAGER_ROLE())
+        await acl.grantPermission(holder, datastore.address, await datastore.DATASTORE_MANAGER_ROLE())
+
+        datastoreACL = await DatastoreACL.new()   
+        await datastoreACL.initialize(datastore.address) 
+        await datastore.init(datastoreACL.address, { from: root })
+    })
+
+    it('increases lastFileId by 1 after addFile', async () => {
+        assert.equal(await datastore.lastFileId(), 0)
+        await datastore.addFile("QmWWQSuPMS6aXCbZKpEjPHPUZN2NjB3YrhJTHsV4X3vb2t", "file name", 100, true)
+        assert.equal(await datastore.lastFileId(), 1)
+    })    
+
+
+})
+
+async function assertThrow(fn) {
+    try {
+        await fn()
+    } catch(e) {
+        return true
+    }
+    assert.fail('Should have thrown')
+}
+
+async function assertEvent(contract, filter) {
+    return new Promise((resolve, reject) => {
+        if (!contract[filter.event])
+            return reject(`No event named ${filter.event} found`)
+
+        const event = contract[filter.event]()
+        event.watch()
+        event.get((error, logs) => {
+            if (error)
+                return reject(`Error while filtering events for ${filter.event}: ${e.message}`)
+
+            const log = _.filter(logs, filter)
+
+            if (log) 
+                resolve(log)
+            else {
+                assert.fail(`Failed to find filtered event for ${filter.event}`)
+                reject()
+            }
+            
+        })
+        event.stopWatching()
+    })
+}
