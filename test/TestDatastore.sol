a69e88f7c3548193d93231c8730ecdff083ff0a0
Mathew Cormier <mathew.corm@gmail.com>
2018-10-14 15:42:23 -0400

    Add apmNamehash to TestDatastore

--- test/TestDatastore.sol
+++ test/TestDatastore.sol
@@ -3,19 +3,11 @@ pragma solidity 0.4.24;
 import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
 import "@aragon/os/contracts/factory/DAOFactory.sol";
 import "@aragon/os/contracts/acl/ACL.sol";
+import "@aragon/os/contracts/apm/APMNamehash.sol";
 
-// You might think this file is a bit odd, but let me explain.
-// We only use the ACL contract in our tests, which
-// means Truffle will not compile it for us, because it is
-// from an external dependency.
-//
-// We are now left with three options:
-// - Copy/paste the MiniMeToken contract
-// - Run the tests with `truffle compile --all` on
-// - Or trick Truffle by claiming we use it in a Solidity test
-//
-// You know which one I went for.
 
-contract TestDatastore {
-  // ...
+contract TestDatastore is APMNamehash {
+    function apmNamehash(string name) external pure returns (bytes32) {
+        return super.apmNamehash(name);
+    }
 }
